4ec54777408a70468c2e97c00e9a51df2d88af6a
KamilKhair <KamilK@code-college.net>
2016-08-13 22:09:02 +0300

    Lab 7.3: Limited Queue

---
+++ Lab7.3/Queues/Queues/LimitedQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Queues
+{
+    public class LimitedQueue<T>
+    {
+        private readonly Queue<T> _queue;
+        private readonly SemaphoreSlim _semaphore;
+        private readonly object _syncLock = new object();
+
+        public LimitedQueue(Queue<T> queue, int maxQueueSize)
+        {
+            MaxSize = maxQueueSize;
+            _queue = queue;
+            _semaphore = new SemaphoreSlim(MaxSize);
+        }
+
+        public int MaxSize { get; private set; }
+
+        public void Enqueue(T element)
+        {
+            _semaphore.Wait();
+            lock (_syncLock)
+            {
+                _queue.Enqueue(element);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Enqueue");
+                Console.WriteLine($"Current queue size: {_queue.Count}");
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (_queue.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty");
+            }
+            _semaphore.Release();
+            T element;
+            lock (_syncLock)
+            {
+                element = _queue.Dequeue();
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} Dequeue");
+                Console.WriteLine($"Current queue size: {_queue.Count}");
+            }
+            return element;
+        }
+    }
+}
---
+++ Lab7.3/Queues/Queues/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Queues
+{
+    public class Program
+    {
+        private readonly LimitedQueue<int> _limitedQueue = new LimitedQueue<int>(new Queue<int>(), 20);
+        public static void Main(string[] args)
+        {
+            for (var i = 0; i < 1000000; ++i)
+            {
+                var program = new Program();
+                ThreadPool.QueueUserWorkItem(program.ThreadEnqueue, i);
+                ThreadPool.QueueUserWorkItem(program.ThreadDequeue, i);
+            }
+            for (var i = 0; i < 1000000; ++i)
+            {
+                var program = new Program();
+            }
+            Console.ReadLine();
+        }
+
+        public void ThreadEnqueue(object threadContext)
+        {
+            for (var i = 0; i < 10000; ++i)
+            {
+                _limitedQueue.Enqueue((int)threadContext);
+            }
+        }
+
+        public void ThreadDequeue(object threadContext)
+        {
+            for (var i = 0; i < 10000; ++i)
+            {
+                try
+                {
+                    _limitedQueue.Dequeue();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+        }
+    }
+}
