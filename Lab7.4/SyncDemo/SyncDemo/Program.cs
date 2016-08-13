05e9a8aca610f47010ec6c9430f14cde209705a0
KamilKhair <KamilK@code-college.net>
2016-08-13 22:58:40 +0300

    Lab 7.4: Inter-process Synchronization

---
+++ Lab7.4/SyncDemo/SyncDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace SyncDemo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Process {Process.GetCurrentProcess().Id} is starting:");
+            using (var syncFileMutex = new Mutex(false, "MyMutex"))
+            {
+                for (var i = 0; i < 10000; i++)
+                {
+                    try
+                    {
+                        syncFileMutex.WaitOne();
+                        using (var fileWriter = new StreamWriter(@"c:\temp\data.txt", true))
+                        {
+                            fileWriter.WriteLine($"Process {Process.GetCurrentProcess().Id} is writing to the file");
+                        }
+                    }
+                    finally
+                    {
+                        using (var fileWriter = new StreamWriter(@"c:\temp\data2.txt", true))
+                        {
+                            fileWriter.WriteLine($"Process {Process.GetCurrentProcess().Id} is about to release the mutex MyMutex");
+                        }
+                        syncFileMutex.ReleaseMutex();
+                    }
+                }
+                Console.WriteLine("Done !");
+            }
+        }
+    }
+}
